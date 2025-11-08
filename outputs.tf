8097b132b96c7afcca6a85a512ad41c981638cc8
vieira-alexandre <alexandrer0x@hotmail.com>
2025-11-08 13:05:57 -0300

    vm amd

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "vcn_id" { value = module.network-1.vcn_id }
 output "public_subnet_id" { value = module.network-1.public_subnet_id }
-output "instance_id" { value = module.vm-small.instance_id }
-output "instance_public_ip" { value = module.vm-small.public_ip }
-output "instance_image_id" { value = module.vm-small.effective_image_id }
-output "instance_boot_volume_size_gbs" { value = module.vm-small.boot_volume_size_gbs }
+output "instance_id" { value = module.vm-amd.instance_id }
+output "instance_public_ip" { value = module.vm-amd.public_ip }
+output "instance_image_id" { value = module.vm-amd.effective_image_id }
+output "instance_boot_volume_size_gbs" { value = module.vm-amd.boot_volume_size_gbs }
 
 output "backend_bucket" {
   value       = var.backend_bucket
