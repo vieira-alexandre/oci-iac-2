4edad35069a3d151e089f435047a2da5ebf30f45
vieira-alexandre <alexandrer0x@hotmail.com>
2025-11-11 09:01:24 -0300

    remove vms

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,3 +1,3 @@
 output "vcn_id" { value = oci_core_vcn.this.id }
 output "public_subnet_id" { value = oci_core_subnet.public.id }
-output "db_nsg_id" { value = oci_core_network_security_group.db.id }
+# output "db_nsg_id" { value = oci_core_network_security_group.db.id }
